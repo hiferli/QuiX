09c1d647a7f5783c8e7fafbeb36406c50f0aaa13
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-09-22 22:14:12 +0530

    Fetching APIs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import axios from 'axios'
 import './App.css';
+import { QUIZ_DATA } from './Statics/QuizData'
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [questions, setQuestions] = useState({})
+
+	const getQuestions = async () => {
+		const API = QUIZ_DATA.QUIZ_API + 'amount=' + QUIZ_DATA.NUMBER_OF_QUESTIONS;
+		
+		await axios.get(API)
+			.then((response) => setQuestions(response.data))
+			.then(() => console.log(questions))
+			.catch((error) => console.log(error))			
+	}
+
+	useEffect(() => {
+		getQuestions();
+	}, [])
+
+
 	return (
 		<>
-			<h1>Hello World</h1>
+			<h1>Welcome</h1>
 		</>
 	);
 }
---
+++ src/Statics/QuizData.js
@@ -0,0 +1,5 @@
+export const QUIZ_DATA = {
+    QUIZ_API: 'https://opentdb.com/api.php?',
+    NUMBER_OF_QUESTIONS: 15,
+    QUIZ_TIME: 30,
+}
