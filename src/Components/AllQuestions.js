8e86b5550658efe4fff4b716a0add171863f2d26
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-09-26 17:39:37 +0530

    Panel ScrollIntoView Added

--- src/Components/AllQuestions.js
+++ src/Components/AllQuestions.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Question from './Question'
 import Panel from './Panel'
 
 const AllQuestions = ({ questions }) => {
+    const [selected, setSelected] = useState(1);
+
+    useEffect(() => {
+        var element = document.getElementById(selected);
+        
+        if(element){
+            element.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+        } else {
+            console.log('None')
+        }
+    }, [selected])
+
     return (
         <div>
             <h1>All the best!</h1>
-            <Panel questions={questions.results} />
+            <Panel id={0} questions={questions.results} setSelected={setSelected} />
 
             {
                 // console.log(questions)
+                // Continue from here to make highlight for each question.
                 questions.results.map(
                     (question, index) => (
-                        <Question key={index} index={index + 1} question={question} />
+                        <div id={index}>
+                            <Question key={index} index={index + 1} question={question} />
+                        </div>
                     )
                 )
             }
--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { QUIZ_DATA } from '../Statics/QuizData'
 
-const Panel = ({ questions }) => {
+const Panel = ({ questions, setSelected }) => {
     return (
         <>
             {
                 // console.log(questions)
                 questions.map(
                     (question, index) => (
-                        <span style={{ background: question.choice ? QUIZ_DATA.COLOR.MARKED : QUIZ_DATA.COLOR.UNMARKED, color: 'white' }}>
+                        <span onClick={() => setSelected(index)} style={{ background: question.choice ? QUIZ_DATA.COLOR.MARKED : QUIZ_DATA.COLOR.UNMARKED, color: 'white' }}>
                             {index}&nbsp;
                         </span>
                     )
