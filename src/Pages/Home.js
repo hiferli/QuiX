435953628d50febd651f91e2cd010d8cae4cb9e6
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-10-11 18:16:09 +0530

    Designing Home Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import { QUIZ_DATA } from '../Statics/QuizData'
 
 const Home = ({email , setEmail}) => {
     let navigate = useNavigate(); 
@@ -27,23 +28,31 @@ const Home = ({email , setEmail}) => {
 
     return (
         <>
-            <h1>Welcome to Quix</h1>
-            <h3>Challenge your ability</h3>
+            <h1 className='mt-9 text-4xl '>Welcome to</h1>
+            <h1 className='text-9xl text-rose-950 font-serif font-black'>Quix</h1>
+            <br />
+            <h3 className='text-xl '>Challenge your ability</h3>
 
-            <div className="rules">
-                <ol>
-                    <li>Rule 1</li>
-                    <li>Rule 2</li>
-                    <li>Rule 3</li>
-                    <li>Rule 4</li>
+            <br /><br />
+            <div className="rules m-2">
+                <h2 className='text-4xl font-black text-red-500 text-transform: uppercase'>Rules</h2>
+                <ol className='list-decimal'>
+                    <li>You are required to enter your email address for starting the quiz. This email would be used to email your result to you as well as to the quiz creater</li>
+                    <li>The quiz has total <span className='font-bold'>{QUIZ_DATA.NUMBER_OF_QUESTIONS}</span> questions waiting for you. You need to solve all of them (Atleast try to) solve all of them in a span of <span className='font-bold'>{QUIZ_DATA.QUIZ_TIME.MINUTES} minutes</span>.</li>
+                    <li>You can use the <span className='font-bold text-green-500'>Submit</span> button to submit the test. Alternatively, <span className='font-bold text-red-500'>the test would be auto-submitted once the time is over</span></li>
+                    <li className='text-orange-600 '>Crying is allowed, but please do so <i>quietly</i>.</li>
                 </ol>
             </div>
 
-            <h2>To proceed, please enter your Email Address</h2>
+            <br /><br />
+            <h2 className='font-bold text-3xl'>To proceed, please enter your Email Address</h2>
 
             <form onSubmit={handleForm}>
-                <input type="email" name="email" id="email" onChange={(event) => setEmail(event.target.value)} />
-                <button type='submit'>Proceed Ahead</button>
+                <br />
+                <input className='px-5  border-2 rounded-md border-slate-300 hover:border-gray-400' placeholder='name@email.com' type="email" name="email" id="email" onChange={(event) => setEmail(event.target.value)} />
+                <br />
+                <button className='m-3 bg-gray-600 hover:bg-white hover:text-gray-600 text-white py-1 px-4 border border-gray-400 rounded shadow' type='submit'>Proceed Ahead</button>
+
             </form>
         </>
     );
